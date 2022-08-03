04ebd5c43c98d56b1e1f43c54c491e335429d11e
Евгения <jane_79@list.ru>
2022-08-03 22:58:51 +0300

    second commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,13 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
-        userService.saveUser("Ahgh", "jhg", (byte) 56);
-        List<User> list = userService.getAllUsers();
-        System.out.printf("");
+        userService.createUsersTable();
     }
 }
